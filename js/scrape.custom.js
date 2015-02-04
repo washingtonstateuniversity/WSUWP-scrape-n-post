f23f64fbde9169b53d68a6892c4340a2e76cb1d2
Jeremy Bass <jeremybass@cableone.net>
2015-02-04 09:12:38 -0800

    appy a simple show hide password for the master post password

--- js/scrape.custom.js
+++ js/scrape.custom.js
@@ -4,5 +4,27 @@
 		$.each($(".radio_buttons"),function(){
 			$(this).buttonset();
 		});
+		
+		
+		$.each($(".show_hide_pass"),function(){
+			var self = $(this);
+			if(self.next('.showhide_btn').length<=0){
+				self.after('<button class="showhide_btn">Show</button>');
+				self.data('showhide',"hidden");
+			}
+			var btn = self.next('.showhide_btn');
+			btn.on('click',function(e){
+				e.preventDefault();
+				var state = self.data('showhide');
+				self.attr('type',(state=="hidden"?"text":"password"));
+				
+				btn.text((state=="hidden"?"Hide":"Show"));
+				self.data('showhide',(state=="hidden"?"":"hidden"));
+			});
+		});
+		
+		
+		
+		
 	});
 })(jQuery);
